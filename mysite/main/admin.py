e1b0f84e9fc865f4a4c6f385ae3b6ad593c18193
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-06-02 14:17:04 +0100

    Added admin site pages and controllers for Recipe and Ingredient models.

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient
 
 # Register your models here.
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    fields = ["recipe_name",
+              "recipe_prep_time",
+              "recipe_prep_time_unit"]
+    list_display = [field.name for field in Recipe._meta.get_fields()]
+    
+
+class IngredientAdmin(admin.ModelAdmin):
+    fields = ["ingredient_name",
+              "ingredient_min_buying_size",
+              "ingredient_count_unit"]
+    list_display = [field.name for field in Ingredient._meta.get_fields()]
+
+
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Ingredient, IngredientAdmin)
