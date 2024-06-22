b0b193b7be7626c85454687dce74374bfa6632be
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-06-22 20:58:23 +0100

    Added manytomany field to relate recipes and ingredients.

--- mysite/main/admin.py
+++ mysite/main/admin.py
@@ -8,16 +8,16 @@ class RecipeAdmin(admin.ModelAdmin):
     """Admin for recipe model."""
     fields = ["recipe_name",
               "recipe_prep_time",
-              "recipe_prep_time_unit"]
-    list_display = [field.name for field in Recipe._meta.get_fields()]
-    
+              "recipe_prep_time_unit",
+              "recipe_ingredients"]
+    list_display = [field.name for field in Recipe._meta.get_fields()][1:-2]
 
 class IngredientAdmin(admin.ModelAdmin):
     """Admin for ingredient model."""
     fields = ["ingredient_name",
               "ingredient_min_buying_size",
               "ingredient_count_unit"]
-    list_display = [field.name for field in Ingredient._meta.get_fields()]
+    list_display = [field.name for field in Ingredient._meta.get_fields()][1:]
 
 
 admin.site.register(Recipe, RecipeAdmin)
--- mysite/main/models.py
+++ mysite/main/models.py
@@ -2,16 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class Recipe(models.Model):
-    """Recipe record in the database. Includes name and prep time with units."""
-    TIME_UNIT = {
-        "min": "minutes",
-        "hr": "hours"
-    }
-    recipe_name = models.CharField(max_length=300)
-    recipe_prep_time = models.IntegerField()
-    recipe_prep_time_unit = models.CharField(max_length=20, choices=TIME_UNIT)
-
 
 class Ingredient(models.Model):
     """
@@ -28,3 +18,15 @@ class Ingredient(models.Model):
     ingredient_name = models.CharField(max_length=100)
     ingredient_count_unit = models.CharField(max_length=10, choices=INGREDIENT_UNIT)
     ingredient_min_buying_size = models.IntegerField()
+
+class Recipe(models.Model):
+    """Recipe record in the database. Includes name and prep time with units."""
+    TIME_UNIT = {
+        "min": "minutes",
+        "hr": "hours"
+    }
+    recipe_name = models.CharField(max_length=300)
+    recipe_prep_time = models.IntegerField()
+    recipe_prep_time_unit = models.CharField(max_length=20, choices=TIME_UNIT)
+    recipe_ingredients = models.ManyToManyField(Ingredient)
+
