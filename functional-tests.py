091477ce4e0e14deb01d34b9ffcee5498a075ed7
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-08-03 10:19:46 +0100

    Edited functional tests to use Python's unittest module and to extend the case to a login test.

--- functional-tests.py
+++ functional-tests.py
@@ -1,7 +1,29 @@
+import unittest
 from selenium import webdriver
 
-# Server test
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self) -> None:
+        self.browser.close()
+
+    def test_can_log_in(self) -> None:
+        """Runs through the process of a user seeing the product and """
+        # David has heard about a meal planning app.
+        # He goes online to the meal planner's homepage.
+        self.browser.get("http://localhost:8000")
+        # He notices that "Home" is in the browser title.
+        assert "Home" in self.browser.title
+        self.assertIn("Home", self.browser.title)
+
+        # He invited to create an account.
+        self.fail("TODO: Finish the test!")
+
+        # He types his name (David) into the Username box and pasword123 into the password box (David doesn't understand cybersecurity).
+
+        # When he hits enter, he logs in and is redirected to his dashboard.
+
+if __name__ == "__main__":
+    unittest.main()
