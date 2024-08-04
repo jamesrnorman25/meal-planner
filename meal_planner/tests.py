36d2e7fecd902f1b1788713313e1c3e22ae2c40f
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-08-04 22:12:17 +0100

    Added test to check 'Create-Account' was rendering the account creation page

--- meal_planner/tests.py
+++ meal_planner/tests.py
@@ -9,3 +9,7 @@ class HomepageTest(TestCase):
     def test_homepage_template_used(self) -> None:
         response = self.client.get('/')
         self.assertTemplateUsed(response, "home.html")
+
+    def test_account_creation_template_used(self) -> None:
+        response = self.client.get('/Create-Account')
+        self.assertTemplateUsed(response, "create_account.html")
