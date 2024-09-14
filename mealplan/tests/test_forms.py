95e8404cb66bb4123ca89e2610eb23ada539fa73
jamesrnorman25 <50826178+13JamesNorman@users.noreply.github.com>
2024-09-14 17:22:14 +0100

    Update to test_forms

--- mealplan/tests/test_forms.py
+++ mealplan/tests/test_forms.py
@@ -1,8 +1,2 @@
 from django.test import TestCase
 
-class NewMealplanTest(TestCase):
-    def setUp(self) -> None:
-        self.response = self.client.get("/mealplans/new")
-
-    def test_uses_correct_template(self) -> None:
-        self.assertTemplateUsed(self.response, "new.html")
