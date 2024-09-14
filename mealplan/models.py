53726de1d2b5dce554ee40639512fdd7ac601cd7
jamesrnorman25 <50826178+13JamesNorman@users.noreply.github.com>
2024-09-14 17:30:00 +0100

    Added Mealplan model and associated migrations.

--- mealplan/models.py
+++ mealplan/models.py
@@ -1,3 +1,12 @@
+"""Models for meal plans."""
 from django.db import models
 
-# Create your models here.
+class Mealplan(models.Model):
+    name = models.CharField(max_length=100, default="")
+    monday = models.CharField(max_length=100, default="")
+    tuesday = models.CharField(max_length=100, default="")
+    wednesday = models.CharField(max_length=100, default="")
+    thursday = models.CharField(max_length=100, default="")
+    friday = models.CharField(max_length=100, default="")
+    saturday = models.CharField(max_length=100, default="")
+    sunday = models.CharField(max_length=100, default="")
--- mealplan/tests/test_models.py
+++ mealplan/tests/test_models.py
@@ -1,6 +1,20 @@
 """Tests Mealplan model and its associated form."""
 
 from django.test import TestCase
+from mealplan.models import Mealplan
 
 class MealplanModelTest(TestCase):
-
+    def test_can_save_and_retrieve_mealplans(self) -> None:
+        mealplan = Mealplan()
+        mealplan.name = "Test Mealplan"
+        mealplan.monday = "Monday's meal"
+        mealplan.tuesday = "Tuesday's meal"
+        mealplan.wednesday = "Wednesday's meal"
+        mealplan.thursday = "Thursday's meal"
+        mealplan.friday = "Friday's meal"
+        mealplan.saturday = "Saturday's meal"
+        mealplan.sunday = "Sunday's meal"
+        mealplan.save()
+        saved_items = Mealplan.objects.all()
+        self.assertEqual(saved_items.count(), 1)
+        self.assertEqual(saved_items[0].name, "Test Mealplan")
