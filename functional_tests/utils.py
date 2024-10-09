370ba4ddedfd423a763ec8bb3f71933b99102776
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-10-09 21:03:10 +0100

    Finished docstring for wait_for function.

--- functional_tests/utils.py
+++ functional_tests/utils.py
@@ -2,7 +2,7 @@ from selenium.common.exceptions import WebDriverException
 import time
 
 def wait_for(function, max, step):
-    """Taken from book 'Obey the Testing Goat' by """
+    """Taken from book 'Obey the Testing Goat' by Harry Percival"""
     start_time = time.time()
     while True:
         try:
