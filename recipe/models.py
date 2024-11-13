feece0789a5b4a03ff8826465aaf0e32ee26c74e
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-11-13 20:54:10 +0000

    Added Recipe and Ingredient models to recipe app.

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=100)
--- recipe/tests/test_models.py
+++ recipe/tests/test_models.py
@@ -1,10 +1,26 @@
 from django.test import TestCase
-from recipe.models import Recipe
+from recipe.models import Recipe, Ingredient
 
 # Create your tests here.
 
-class TestRecipeModel(TestCase):
+class RecipeModelTest(TestCase):
     def setUp(self) -> None:
-        recipe = Recipe()
-        recipe.name = "Tuna Sandwich"
-        recipe.method = "Lorem ipsum dolor sit amet."
+        self.recipe = Recipe()
+        self.recipe.name = "Tuna Sandwich"
+        self.recipe.method = "Lorem ipsum dolor sit amet."
+        self.recipe.save()
+
+    def test_can_save_model(self) -> None:
+        recipes = Recipe.objects.all()
+        self.assertIn(self.recipe, recipes)
+
+
+class IngredientModelTest(TestCase):
+    def setUp(self) -> None:
+        self.ingredient = Ingredient()
+        self.ingredient.name = "Ingredient 1"
+        self.ingredient.save()
+
+    def test_can_save_model(self) -> None:
+        ingredients = Ingredient.objects.all()
+        self.assertIn(self.ingredient, ingredients)
