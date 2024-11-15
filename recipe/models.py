d622d3c99e518c76fb43d07c991bf7f5d5efc89c
jamesrnorman25 <jamesrnorman25@gmail.com>
2024-11-15 18:55:25 +0000

    Added Ingredient and Recipe models.

--- recipe/models.py
+++ recipe/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
-# Create your models here.
-
-class Recipe(models.Model):
-    name = models.CharField(max_length=100)
-
+# Create your models here.#
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=100)
+    method = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient)
+
--- recipe/tests/test_models.py
+++ recipe/tests/test_models.py
@@ -1,19 +1,25 @@
 from django.test import TestCase
 from recipe.models import Recipe, Ingredient
 
+import logging
+logger = logging.getLogger(__name__)
+
 # Create your tests here.
 
 class RecipeModelTest(TestCase):
+   
     def setUp(self) -> None:
-        self.recipe = Recipe()
-        self.recipe.name = "Tuna Sandwich"
-        self.recipe.method = "Lorem ipsum dolor sit amet."
-        self.recipe.save()
+       self.recipe = Recipe()
+       self.recipe.save()
+   
+    def test_default_name(self) -> None:
+        self.assertEqual(self.recipe.name, "")
 
-    def test_can_save_model(self) -> None:
-        recipes = Recipe.objects.all()
-        self.assertIn(self.recipe, recipes)
+    def test_default_method(self) -> None:
+        self.assertEqual(self.recipe.method, "")
 
+    def test_default_ingredients(self) -> None:
+        self.assertEqual(self.recipe.ingredients.all().count(), 0)
 
 class IngredientModelTest(TestCase):
     def setUp(self) -> None:
