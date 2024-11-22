3a9abcb2832c0d199865025b8721ff7215cc5df0
jamesrnorman25 <50826178+13JamesNorman@users.noreply.github.com>
2024-11-22 22:11:12 +0000

    Edited field for ingredients relationship in recipe form.

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,7 +1,9 @@
-from django.forms import ModelForm
-from recipe.models import Recipe
+from django import forms
+from recipe.models import Recipe, Ingredient
     
-class RecipeForm(ModelForm):
+class RecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
         fields = ("name", "ingredients", "method")
+
+    ingredients = forms.ModelMultipleChoiceField(queryset=Ingredient.objects.all())
