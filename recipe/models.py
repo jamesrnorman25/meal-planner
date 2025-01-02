fccafdd942cb5cd96fae6cd400ebc50ef1002b4b
jamesrnorman25 <50826178+13JamesNorman@users.noreply.github.com>
2025-01-02 15:53:29 +0000

    Added slug and user fields to Recipe model.

--- recipe/models.py
+++ recipe/models.py
@@ -1,16 +1,26 @@
 from django.db import models
-
-# Create your models here.#
+from django.contrib.auth.models import User
+from django.template.defaultfilters import slugify
+from time import time
+# Create your models here.
 
 
 class Ingredient(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     method = models.TextField()
     ingredients = models.ManyToManyField(Ingredient, through="RecipeIngredient")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    slug = models.SlugField()
+
+    def save(self):
+        self.slug = slugify(f"{self.name} {time()}")
 
 
 class RecipeIngredient(models.Model):
