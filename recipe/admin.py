5fef13e0851a08c95fc65b396db0cb8d6a9e648f
jamesrnorman25 <50826178+13JamesNorman@users.noreply.github.com>
2025-01-27 12:06:35 +0000

    Registered RecipeIngredient model on admin page.

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from recipe.models import Ingredient, Recipe
+from recipe.models import Ingredient, Recipe, RecipeIngredient
 
 # Register your models here.
 class IngredientAdmin(admin.ModelAdmin):
@@ -11,5 +11,10 @@ class RecipeAdmin(admin.ModelAdmin):
     list_display=("name",)
     fields=["name", "method"]
 
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display=("recipe", "ingredient", "quantity")
+    fields=["recipe", "ingredient", "quantity"]
+
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(RecipeIngredient, RecipeIngredientAdmin)
